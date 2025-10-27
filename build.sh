11beff98727b836790f66188d13026e74acc0a90
SwuduSusuwu <2002SwuduSusuwu@gmail.com>
2025-10-26 18:52:49 -0700

    @`./build.sh`: `java --source susuwu/FishSim.java`
    Is followup to: commit HEAD~3 (@`./susuwu/FishSim.java`: Simple *Java* fish sim). TODO: `squash` this?
    @`./build.sh`: simple build script for `./susuwu/FishSim.java`, which allows unit tests through *GitHub Workflows*
    	+`APTITUDE="apt"` Purpose: simple to set whole script to use alternative installer-executables.
    	+`PATH_TO_CLASS`: is not prefixed with `./`, since thus gives "Error: could not find or load main class ..FishSim". This prefix is used so that local paths are not confused with URLs (which both use `<name>.<extension>`).
    	+`JAVA_MODULES`: to deduplicate paths, stores modules which `java*` reuses.
    	+`JAVA_FLAGS`: to deduplicate options, stores options which `java` reuses.
    		@`JAVA_FLAGS`: +`-enableassertions` is now included, so `assert` in source code is used. Simple to disable (just comment the row) so performance improves.
    	+`JAVA_BUILD_TEST_FLAGS="-verbose"`: to deduplicate options, stores options which unit test build reuses.
    		use `+="--source 16"` for "error: an enum switch case label must be the unqualified name of an enumeration constant" solution (notice: gives "error: invalid source release: 16" on GitHub workflows).
    	+`JAVA_TEST_FLAGS="-verbose:module"`: to deduplicate options, stores options which unit test execution reuses.
    
    @`./posts/FishSim.md#intro`: references `./susuwu/build.sh`
    +`./susuwu/build.sh`: fixes "Error: Could not find or load main class Java.FishSim" for compiled execution.
    	TODO: Research reasons which `java` gives such error messages (for execution from subdirs unless `java --source` is used).

--- build.sh
+++ build.sh
@@ -1,3 +1,42 @@
 #!/bin/sh
-return 0 #Placeholder, so [CodeQL](https://docs.github.com/en/code-security/code-scanning/introduction-to-code-scanning/about-code-scanning-with-codeql) passes.
+#
+# /* This is the new build script for `./susuwu/FishSim.java`. */
+# /* Attribution (henceforth "*this attribution*", whose syntax is *Markdown*): 2024 [Swudu Susuwu](https://swudususuwu.substack.com)
+#  * <https://github.com/SwuduSusuwu/SusuJava/> has the newest version of `./build.sh` (henceforth "*this source code*").
+#  * If *this attribution* is shown, *this source code* allows all uses. *This attribution* constitutes the most permissive which is compatible with [*GPLv2*](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html) + [*Apache 2*](https://www.apache.org/licenses/LICENSE-2.0.html), which is suitable for personal use (also suitable for school use).
+#  * If *this attribution* is not professional enough for business use: businesses can use *this source code* through included versions of [*GPLv2*](./LICENSE_GPLv2), [*Apache 2*](./LICENSE), or through both of those. */
+
+PATH_TO_FX="${PATH_TO_FX:-"/usr/share/openjfx/lib/"}" # /* Notice: prefix " "s in path with slashes, such as "\ ". */
+PATH_TO_CLASS="susuwu/FishSim"
+PATH_TO_SOURCE="${PATH_TO_CLASS}.java"
+JAVA_MODULES="${JAVA_MODULES} --module-path ${PATH_TO_FX} --add-modules javafx.controls,javafx.fxml" # /* Notice: quotes around `${PATH_TO_FX}` give errors, so ensure to escape the path in `PATH_TO_FX=...` */
+#JAVA_MODULES="${JAVA_MODULES} --module-path \"${PATH_TO_FX}\" --add-modules javafx.controls,javafx.fxml" # /* Notice: those quotes (around `${PATH_TO_FX}`) give "java.lang.module.FindException: Module javafx.controls not found" */
+JAVA_FLAGS="${JAVA_FLAGS} --enable-native-access=javafx.graphics"
+JAVA_FLAGS="${JAVA_FLAGS} -enableassertions" # /* Notice: remove `-enableassertions` so performance improves */
+export JAVA_BUILD_TEST_FLAGS="-verbose"
+export JAVA_TEST_FLAGS="-verbose:module"
+if command -v sudo >/dev/null; then
+	APTITUDE="sudo apt -y install "
+else
+	APTITUDE="apt -y install " # /* Fixes "The program sudo is not installed." on smartphones */
+fi
+if ! test -d "${PATH_TO_FX}"; then # /* TODO: search for this if default (**Ubuntu**'s) path is not found */
+	${APTITUDE} openjfx || ${APTITUDE} libopenjfx-java
+	if ! test -d "${PATH_TO_FX}"; then
+		echo "$0: '${PATH_TO_FX}' dir not found. Use \`${APTITUDE} install openjfx\`, then set '\${PATH_TO_FX}' to the actual libs."
+	fi
+fi
+command -v java >/dev/null || ${APTITUDE} openjdk-21-jdk-headless || ${APTITUDE} default-jdk-headless
+
+if [ -n "${GITHUB_ACTIONS}" ]; then
+#shellcheck disable=SC2086 # /* Quotes cause "Unrecognized option:" */
+	javac ${JAVA_BUILD_TEST_FLAGS} ${JAVA_MODULES} ${PATH_TO_SOURCE} # /* Gives "Missing JavaFX application class susuwu/FishSim" unless `cd $(dirname ${PATH_TO_CLASS})` is used. */
+#	java ${JAVA_TEST_FLAGS} ${JAVA_FLAGS} ${JAVA_MODULES} ${PATH_TO_CLASS} # /* Assumes `main()` will `return` (but, `FishSim.java`'s does not) */
+#	java ${JAVA_TEST_FLAGS} ${JAVA_FLAGS} --source 16 ${JAVA_MODULES} ${PATH_TO_SOURCE} # /* Assumes `main()` will `return` (but, `FishSim.java`'s does not) */
+else
+#shellcheck disable=SC2086 # /* Quotes cause "Unrecognized option:" */
+	java ${JAVA_FLAGS} --source 16 ${JAVA_MODULES} ${PATH_TO_SOURCE} # /* `---source` is workaround for "Missing JavaFX application class susuwu/FishSim" */
+fi
+
+exit $? #Status required so [*CodeQL*](https://docs.github.com/en/code-security/code-scanning/introduction-to-code-scanning/about-code-scanning-with-codeql) passes.
 
--- susuwu/build.sh
+++ susuwu/build.sh
@@ -1,10 +1,44 @@
 #!/bin/sh
 #
-# This is the [original build script from *Solar-Pro-2*](https://poe.com/s/ehlOJYRJNsrGJttfJ4HK), plus braces plus quotes (so `shellcheck` passes), published for historical value.
-if ! test -d "${PATH_TO_FX}"; then
-	echo "$0: Make sure to replace \`\${PATH_TO_FX}\` with the actual path to your JavaFX SDK."
+# /* This is the new build script for `./susuwu/FishSim.java`. Notice: must execute this from `./susuwu/`. */
+# /* Attribution (henceforth "*this attribution*", whose syntax is *Markdown*): 2024 [Swudu Susuwu](https://swudususuwu.substack.com)
+#  * <https://github.com/SwuduSusuwu/SusuJava/> has the newest version of `./susuwu/build.sh` (henceforth "*this source code*").
+#  * If *this attribution* is shown, *this source code* allows all uses. *This attribution* constitutes the most permissive which is compatible with [*GPLv2*](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html) + [*Apache 2*](https://www.apache.org/licenses/LICENSE-2.0.html), which is suitable for personal use (also suitable for school use).
+#  * If *this attribution* is not professional enough for business use: businesses can use *this source code* through included versions of [*GPLv2*](./LICENSE_GPLv2), [*Apache 2*](./LICENSE), or through both of those. */
+
+PATH_TO_FX="${PATH_TO_FX:-"/usr/share/openjfx/lib/"}" # /* Notice: prefix " "s in path with slashes, such as "\ ". */
+PATH_TO_CLASS="FishSim"
+PATH_TO_SOURCE="${PATH_TO_CLASS}.java"
+JAVA_MODULES="${JAVA_MODULES} --module-path ${PATH_TO_FX} --add-modules javafx.controls,javafx.fxml" # /* Notice: quotes around `${PATH_TO_FX}` give errors, so ensure to escape the path in `PATH_TO_FX=...` */
+#JAVA_MODULES="${JAVA_MODULES} --module-path \"${PATH_TO_FX}\" --add-modules javafx.controls,javafx.fxml" # /* Notice: those quotes (around `${PATH_TO_FX}`) give "error: module not found: javafx.{fxml, controls}", so this version is not used. */
+JAVA_FLAGS="${JAVA_FLAGS} --enable-native-access=javafx.graphics"
+JAVA_FLAGS="${JAVA_FLAGS} -enableassertions" # /* Notice: remove `-enableassertions` so performance improves */
+export JAVA_BUILD_TEST_FLAGS="-verbose"
+export JAVA_TEST_FLAGS="-verbose:module"
+if command -v sudo >/dev/null; then
+	APTITUDE="sudo apt -y install "
+else
+	APTITUDE="apt -y install " # /* Fixes "The program sudo is not installed." on smartphones */
 fi
-javac --module-path "${PATH_TO_FX}" --add-modules javafx.controls,javafx.fxml FishSim.java
-java --module-path "${PATH_TO_FX}" --add-modules javafx.controls,javafx.fxml FishSim
-exit $?
+if ! test -d "${PATH_TO_FX}"; then # /* TODO: search for this if default (**Ubuntu**'s) path is not found */
+	${APTITUDE} openjfx || ${APTITUDE} libopenjfx-java
+	if ! test -d "${PATH_TO_FX}"; then
+		echo "$0: '${PATH_TO_FX}' dir not found. Use \`${APTITUDE} install openjfx\`, then set '\${PATH_TO_FX}' to the actual libs."
+	fi
+fi
+command -v java >/dev/null || ${APTITUDE} openjdk-21-jdk-headless || ${APTITUDE} default-jdk-headless
+
+if [ -n "${GITHUB_ACTIONS}" ]; then
+#shellcheck disable=SC2086 # /* Quotes cause "Unrecognized option:" */
+	javac ${JAVA_BUILD_TEST_FLAGS} ${JAVA_MODULES} ${PATH_TO_SOURCE} # /* Gives "error: cannot find symbol\n...\n  symbol:   class Force" */
+#	java ${JAVA_TEST_FLAGS} ${JAVA_FLAGS} ${JAVA_MODULES} ${PATH_TO_CLASS} # /* Assumes `main()` will `return` (but, `FishSim.java`'s does not) */
+#	java ${JAVA_TEST_FLAGS} ${JAVA_FLAGS} --source 16 ${JAVA_MODULES} ${PATH_TO_SOURCE} # /* Assumes `main()` will `return` (but, `FishSim.java`'s does not) */
+else
+#	javac ${JAVA_MODULES} ${PATH_TO_SOURCE} # /* Gives "error: cannot find symbol\n...\n  symbol:   class Force" */
+#	java ${JAVA_FLAGS} ${JAVA_MODULES} ${PATH_TO_CLASS}
+#shellcheck disable=SC2086 # /* Quotes cause "Unrecognized option:" */
+	java ${JAVA_FLAGS} --source 16 ${JAVA_MODULES} ${PATH_TO_SOURCE} # /* `---source` is workaround for "error: cannot find symbol\n...\n  symbol:   {class Force,variable Utils}" */
+fi
+
+exit $? #Status required so [*CodeQL*](https://docs.github.com/en/code-security/code-scanning/introduction-to-code-scanning/about-code-scanning-with-codeql) passes.
 
