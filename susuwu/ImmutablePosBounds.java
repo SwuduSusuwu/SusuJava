c57a51a5e7c1e998e5fa358c85bec43cdee2bcf6
SwuduSusuwu <2002SwuduSusuwu@gmail.com>
2025-12-27 11:55:18 -0800

    @`ImmutablePosBounds.java`: `s/double[]/Pos/`
    Is followup to: commit 90427117512b8b34b3afb1b0a8f580bf63098f8a (+`ImmutablePosBounds.java`: @`package susuwu;`).
    Is followup to: commit HEAD~6 (@`FishSim.java`: split into +`Pos.java`).

--- susuwu/ImmutablePosBounds.java
+++ susuwu/ImmutablePosBounds.java
@@ -5,7 +5,6 @@
  */
 
 package susuwu; /* Usage: `import susuwu.ImmutablePosBounds;` */
-import susuwu.Calculus; /* `Calculus.volume()` */
 import java.util.Arrays;
 /**
  * `class ImmutablePosBounds` says how sims enforce position bounds.
@@ -23,9 +22,10 @@ public class ImmutablePosBounds implements java.lang.Cloneable { /* Usage: repla
 		boundless, // `pos[dim] += dpos[dim];`.
 	}; // Notice: to teleport to new positions, `dpos[dim] = newPos[dim] - pos[dim]`. but most sims use relative motions.
 	protected PosBoundsMode posBounds;
-	protected double[] bounds = { 0, 0 };
-	protected double[] boundsSlash2 = { bounds[0] / 2, bounds[1] / 2 }; // Improves execution of inner loops which use this
-	protected double boundsVolume = boundsSlash2[0] * boundsSlash2[1];
+
+	protected Pos bounds = new Pos(0, 0);
+	protected Pos boundsSlash2 = bounds.slashScalar(2); // Improves execution of inner loops which use this
+	protected double boundsVolume = bounds.volume();
 	protected int gridResolution = 100;
 	protected int[] gridSize = computeGridSize();
 	public ImmutablePosBounds() {
@@ -38,13 +38,17 @@ public class ImmutablePosBounds implements java.lang.Cloneable { /* Usage: repla
 		setPosBoundsMode_(o);
 		setResolution_(resolution);
 	}
+	public ImmutablePosBounds(PosBoundsMode o, ImmutablePos resolution) {
+		setPosBoundsMode_(o);
+		setResolution_(resolution);
+	}
 	@Override
 	public PosBounds clone() { /* Usage: `ImmutablePosBounds pos = o.clone(); assert o.equals(pos);` */
 		return new PosBounds(this);
 	}
 	protected int[] computeGridSize() {
 //		int[] gridSize_ = bounds.clone();
-		int[] gridSize_ = new int[bounds.length];
+		int[] gridSize_ = new int[bounds.dims()];
 		for(int i = 0; gridSize_.length > i; ++i) {
 			gridSize_[i] = (int)Math.ceil(getBounds(i) / gridResolution);
 		}
@@ -57,11 +61,12 @@ public class ImmutablePosBounds implements java.lang.Cloneable { /* Usage: repla
 	}
 	protected void setResolution_(double[] resolution) {
 		setBounds_(resolution);
-		for(int index = 0; resolution.length > index; ++index) {
-			setBoundsSlash2_(index, getBounds(index) / 2);
-		}
-		boundsVolume = Calculus.volume(resolution);
+		boundsSlash2 = bounds.slashScalar(2);
+		boundsVolume = bounds.volume();
 		gridSize = computeGridSize();
+	}
+	protected void setResolution_(ImmutablePos resolution) {
+		setResolution_(resolution.pos);
 	}
 	protected void set_(ImmutablePosBounds o) {
 		setPosBoundsMode_(o.posBounds);
@@ -70,17 +75,23 @@ public class ImmutablePosBounds implements java.lang.Cloneable { /* Usage: repla
 	}
 	protected void setBounds_(double[] resolution) {
 //		assert null != resolution; // Notice: chose to use `java`'s default solution for `clone(null)`
+		bounds = new Pos(resolution);
+	}
+	protected void setBounds_(ImmutablePos resolution) {
 		bounds = resolution.clone();
 	}
 	protected void setBounds_(int index, double resolution) {
-		bounds[index] = resolution;
+		bounds.set(index, resolution);
 	}
 	protected void setBoundsSlash2_(double[] resolution) {
 //		assert null != resolution; // Notice: chose to use `java`'s default solution for `clone(null)`
+		boundsSlash2 = new Pos(resolution);
+	}
+	protected void setBoundsSlash2_(ImmutablePos resolution) {
 		boundsSlash2 = resolution.clone();
 	}
 	protected void setBoundsSlash2_(int index, double resolution) {
-		boundsSlash2[index] = resolution;
+		boundsSlash2.set(index, resolution);
 	}
 	protected void setGridResolution_(int resolution) {
 		gridResolution = resolution;
@@ -91,19 +102,31 @@ public class ImmutablePosBounds implements java.lang.Cloneable { /* Usage: repla
 	public PosBoundsMode getPosBoundsMode() { return this.posBounds; }
 	public double[] getBounds() {
 		assert null != bounds;
-		return bounds; // Notice: should `return bounds.clone()` to enforce "Immutable", but this is used in inner loops
-	}
-	public double getBounds(int index) {
-//		assert null != bounds; // Notice: chose to use `java`'s default `throw` for `double[null]`
-		return bounds[index];
+		assert null != bounds.pos;
+		return bounds.pos; // Notice: should `return bounds.pos.clone()` to enforce "Immutable", but this is used in inner loops
 	}
 	public double[] getBoundsSlash2() { // Caches `getBounds()[dim] / 2` for physics uses (improves inner loops).
 		assert null != boundsSlash2;
+		assert null != boundsSlash2.pos;
+		return boundsSlash2.pos; // Notice: should `return boundsSlash2.pos.clone()` to enforce "Immutable", but this is used in inner loops
+	}
+	public ImmutablePos getBoundsPos() {
+		assert null != bounds; // Notice:  redundant (since `java` checks for `null` dereferences), but documents what this does
+		return bounds;
+	}
+	public double getBounds(int index) {
+//		assert null != bounds; // Notice: chose to use `java`'s default `throw`
+//		assert null != bounds.pos; // Notice: chose to use `java`'s default `throw`
+		return bounds.pos[index];
+	}
+	public ImmutablePos getBoundsSlash2Pos() { // Caches `getBounds().slash(2)` for physics uses (improves inner loops).
+		assert null != boundsSlash2; // Notice:  redundant (since `java` checks for `null` dereferences), but documents what this does
 		return boundsSlash2; // Notice: should `return boundsSlash2.clone()` to enforce "Immutable", but this is used in inner loops
 	}
 	public double getBoundsSlash2(int index) {
-//		assert null != bounds; // Notice: chose to use `java`'s default `throw` for `double[null]`
-		return boundsSlash2[index];
+//		assert null != bounds; // Notice: chose to use `java`'s default `throw`
+//		assert null != bounds.pos; // Notice: chose to use `java`'s default `throw`
+		return boundsSlash2.pos[index];
 	}
 	public double getBoundsVolume() {
 		return boundsVolume;
@@ -137,18 +160,21 @@ public class ImmutablePosBounds implements java.lang.Cloneable { /* Usage: repla
 
 	/* Public `ImmutablePosBounds` functions, which accept `double[]` (future versions also accept `ImmutablePos`): */
 	public String posOutOfBoundsStr(double[] pos, String posStr) {
-		return "`" + posStr + " = " + Arrays.toString(pos) + ";` `getBounds() = " + Arrays.toString(getBounds()) + ";` (`grid = new ArrayList[" + gridSize[0] + "][" + gridSize[1] + "];`), so `" + posStr + "` is out of bounds.";
+		return "`" + posStr + " = " + Arrays.toString(pos) + ";` `getBounds() = " + bounds.toString() + ";` (`grid = new ArrayList[" + gridSize[0] + "][" + gridSize[1] + "];`), so `" + posStr + "` is out of bounds.";
+	}
+	public String posOutOfBoundsStr(ImmutablePos pos, String posStr) {
+		return "`" + posStr + " = " + Arrays.toString(pos.pos) + ";` `getBounds() = " + bounds.toString() + ";` (`grid = new ArrayList[" + gridSize[0] + "][" + gridSize[1] + "];`), so `" + posStr + "` is out of bounds.";
 	}
 
 	public double[] posDiff(double[] pos, double[] o) {
-		if(PosBounds.PosBoundsMode.wrapAroundResolution == getPosBoundsMode()) {
+		if(PosBoundsMode.wrapAroundResolution == getPosBoundsMode()) {
 			double[] posDiff = new double[pos.length];
 			for(int i = 0; pos.length > i; ++i) { /* Notice: ensure that `java` [unrolls this](https://github.com/SwuduSusuwu/SusuPosts/blob/preview/posts/Physics_sims_which_structures_to_use.md#separate-variables-versus-dim-lists) */
 				posDiff[i] = pos[i] - o[i];
-				if(getBoundsSlash2()[i] < posDiff[i]) {
-					posDiff[i] -= getBounds()[i];
-				} else if(-getBoundsSlash2()[i] > posDiff[i]) {
-					posDiff[i] += getBounds()[i];
+				if(boundsSlash2.pos[i] < posDiff[i]) {
+					posDiff[i] -= bounds.pos[i];
+				} else if(-boundsSlash2.pos[i] > posDiff[i]) {
+					posDiff[i] += bounds.pos[i];
 				}
 			}
 			return posDiff;
@@ -156,16 +182,40 @@ public class ImmutablePosBounds implements java.lang.Cloneable { /* Usage: repla
 			return new double[] {pos[0] - o[0], pos[1] - o[1]};
 		}
 	}
+	public Pos posDiff(ImmutablePos pos, ImmutablePos o) {
+		Pos posDiff = pos.minus(o);
+		if(PosBoundsMode.wrapAroundResolution == getPosBoundsMode()) {
+			for(int i = 0; pos.pos.length > i; ++i) { /* Notice: ensure that `java` [unrolls this](https://github.com/SwuduSusuwu/SusuPosts/blob/preview/posts/Physics_sims_which_structures_to_use.md#separate-variables-versus-dim-lists) */
+				if(boundsSlash2.pos[i] < posDiff.pos[i]) {
+					posDiff.pos[i] -= bounds.pos[i];
+				} else if(-boundsSlash2.pos[i] > posDiff.pos[i]) {
+					posDiff.pos[i] += bounds.pos[i];
+				}
+			}
+		}
+		return posDiff;
+	}
 
 	public boolean isPosInBounds(double[] pos) throws IllegalArgumentException {
-		if(getBounds().length != pos.length) {
-			throw new IllegalArgumentException("`getBounds().length != pos.length`");
+		if(bounds.dims() != pos.length) {
+			throw new IllegalArgumentException("`bounds.dims() != pos.length`");
 		} // TODO: If this test is used at the start of all `pos*()` functions, replace `[]` with `Pos2`, unless optimizer stores this.
 		for(int i = 0; i < pos.length; i++) {
-			if(0 > pos[i] || getBounds()[i] <= pos[i]) {
+			if(0 > pos[i] || bounds.at(i) <= pos[i]) {
 				return false;
 			}
 		} // TODO: replace `for(...) {...}` with `switch(pos.length) { case 2: ... }`, unless optimizer does this.
+		return true;
+	}
+	public boolean isPosInBounds(ImmutablePos pos) throws IllegalArgumentException {
+		if(getBoundsPos().dims() != pos.dims()) {
+			throw new IllegalArgumentException("`getBoundsPos().dims() != pos.dims()`");
+		} // TODO: If this test is used at the start of all `pos*()` functions, replace `[]` with `Pos2`, unless optimizer stores this.
+		for(int i = 0; i < pos.dims(); i++) {
+			if(0 > pos.pos[i] || bounds.pos[i] <= pos.pos[i]) {
+				return false;
+			}
+		} // TODO: replace `for(...) {...}` with +`ImmutablePos::isInBounds(ImmutablePos bounds)`, which can use `switch(pos.length) { case 2: ... }` or specialized virtual functions
 		return true;
 	}
 
@@ -178,12 +228,36 @@ public class ImmutablePosBounds implements java.lang.Cloneable { /* Usage: repla
 			}
 			break;
 		case wrapAroundResolution:
-			pos[0] = (pos[0] + getBounds()[0]) % getBounds()[0];
-			pos[1] = (pos[1] + getBounds()[1]) % getBounds()[1];
+			pos[0] = ((pos[0] % bounds.pos[0]) + bounds.pos[0]) % bounds.pos[0];
+			pos[1] = ((pos[1] % bounds.pos[1]) + bounds.pos[1]) % bounds.pos[1];
 			break;
 		case clampToResolution:
-			pos[0] = Math.max(0, Math.min(getBounds()[0] - 1, pos[0])); // TODO: if `java` does not precompute `getBounds()[dim] - 1`, store `boundsMinus1[]`
-			pos[1] = Math.max(0, Math.min(getBounds()[1] - 1, pos[1]));
+			pos[0] = Math.max(0, Math.min(bounds.pos[0] - 1, pos[0])); // TODO: if `java` does not precompute `bounds.at(dim) - 1`, store `boundsMinus1[]`
+			pos[1] = Math.max(0, Math.min(bounds.pos[1] - 1, pos[1]));
+			break;
+		case boundless:
+			return isPosInBounds(pos);
+		default:
+			throw new IllegalArgumentException("Unknown `enum PosBoundsMode`: " + posBounds); // [The compiler does this for you](https://codingtechroom.com/question/what-exception-compiler-unknown-enum-values-switch-expressions), so this just serves to document the lack of `default` codeflow.
+		}
+		return true;
+	}
+	public boolean posBound(Pos pos) throws IllegalArgumentException { // If `PosBoundsMode.boundless != posBounds`, this ensures the invariant `0 <= pos[dim] && FishSim.getBounds()[dim] > pos[dim]` is established.
+		assert getBoundsPos().dims() == pos.dims(); // Notice: `bounds` for volumetric physics allows 2-dimensional `pos`, but `posBounds` does not implement such codeflow
+		switch(getPosBoundsMode()) { // `PosBoundsMode.` is omitted from all `case`s, to support old `java --source` versions
+		case invalidArgumentException:
+			if(!isPosInBounds(pos)) {
+				throw new IllegalArgumentException(posOutOfBoundsStr(pos, "Pos pos"));
+				// return false; // Notice: unsure of codeflow after the exception is handled. This gives an error if uncommented, but without this, if the exception is handled, the function will fall through to `return true`.
+			}
+			break;
+		case wrapAroundResolution:
+			pos.plusEquals(getBoundsPos());
+			pos.moduloEquals(getBoundsPos());
+			break;
+		case clampToResolution:
+			pos.pos[0] = Math.max(0, Math.min(getBounds()[0] - 1, pos.pos[0])); // TODO: +`Pos::{maxEquals(Pos), minEquals(Pos)}` or +`Pos::clamp(pos)`
+			pos.pos[1] = Math.max(0, Math.min(getBounds()[1] - 1, pos.pos[1]));
 			break;
 		case boundless:
 			return isPosInBounds(pos);
